7967c74e36af881424045dff678e607199ae6c16
Afonso Ferreira Miguel <afonso.miguel@pucpr.br>
2025-05-26 17:57:37 -0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-enum LinkedListNode{
+enum LinkedListNode {
     Nil,
-    Node(i32, Box<LinkedListNode>)
+    Node(i32, Box<LinkedListNode>),
 }
 
-impl LinkedListNode{
-    fn display_list(&self){
-        
+impl LinkedListNode {
+    fn display_list(&self) {
+        match self {
+            Self::Nil => println!("Nil"),
+            Self::Node(v, tail) => {
+                print!("{}->", v);
+                tail.display_list();
+            }
+        }
     }
 }
 
-
 fn main() {
-    println!("Olá mundo");
+    let mut lista = 
+        LinkedListNode::Node(10, 
+            Box::new(LinkedListNode::Nil)
+        );
+    lista.display_list();
 }
