9b4bcc0a2e688247fa63767f838e3eea6938ddb7
Afonso Ferreira Miguel <afonso.miguel@pucpr.br>
2025-05-26 17:50:23 -0300

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+enum LinkedListNode{
+    Nil,
+    Node(i32, Box<LinkedListNode>)
+}
+
+impl LinkedListNode{
+    fn display_list(&self){
+        
+    }
+}
+
+
 fn main() {
     println!("Olá mundo");
 }
